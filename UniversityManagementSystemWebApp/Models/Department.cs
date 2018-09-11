410d3092a43a0356fc3c07406199b5d5467fdf50
Sohel-Mahmud <sohel7avon@gmail.com>
2018-09-11 21:14:18 +0600

    initial test commit
    testing sohel brach

--- UniversityManagementSystemWebApp/Models/Department.cs
+++ UniversityManagementSystemWebApp/Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace UniversityManagementSystemWebApp.Models
 {
     public class Department
     {
+        [StringLength(7,MinimumLength = 2,ErrorMessage = "Code length must be 2 to 7 characters long")]
         public string Code { get; set; }
         public string Name { get; set; }
     }
