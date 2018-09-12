72ab0bc4fdbb66bbb2e7c20663743f39b1e2eeb7
Forman Khan <formankhan2014@gmail.com>
2018-09-12 14:15:22 +0600

    Add AllocateController

---
+++ UniversityManagementSystemWebApp/Controllers/AllocateClassRoomController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UniversityManagementSystemWebApp.Controllers
+{
+    public class AllocateClassRoomController : Controller
+    {
+        //
+        // GET: /AllocateClassRoom/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
