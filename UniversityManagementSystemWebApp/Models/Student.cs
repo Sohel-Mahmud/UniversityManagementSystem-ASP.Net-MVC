d6b8c3260399e5ae3fac2af414108abac2dd293a
Md Azizul Hakim <ahslucky12@gmail.com>
2018-09-16 12:59:09 +0600

    Student Registration Completed
    Student Registration Completed
    proper Validation Added
    Registration number generator working properly

--- UniversityManagementSystemWebApp/Models/Student.cs
+++ UniversityManagementSystemWebApp/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,18 @@ namespace UniversityManagementSystemWebApp.Models
     public class Student
     {
         public int StudentId { get; set; }
+        [Required(ErrorMessage = "Please Provide Student Name!")]
         public string StudentName { get; set; }
+        [Required(ErrorMessage = "Please Provide Contact Number!")]
+        [StringLength(11,MinimumLength = 11,ErrorMessage = "Contact Number Must Be 11 Digit Long!")]
         public string ContactNo { get; set; }
+        [Required(ErrorMessage = "Please Provide Email Address!")]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string  Date { get; set; }
         public string Address { get; set; }
+        [Required(ErrorMessage = "Please Select a Department!")]
         public int DepartmentId { get; set; }
         public string RegistrationNo { get; set; }
 
