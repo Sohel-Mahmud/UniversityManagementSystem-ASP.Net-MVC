dc0eb9f228e2dcbc0aad58c71c423f7d40ab79a7
Md Azizul Hakim <ahslucky12@gmail.com>
2018-10-22 20:11:51 +0600

    Flash
    Flashing curropted master for new marge

--- packages/Bootstrap.v3.Datetimepicker.4.17.45/tools/install.ps1
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-# install.ps1
-$DTE.ItemOperations.Navigate("https://github.com/Eonasdan/bootstrap-datetimepicker/issues/1128", $DTE.vsNavigateOptions.vsNavigateOptionsNewWindow)
-=======
-# install.ps1
-$DTE.ItemOperations.Navigate("https://github.com/Eonasdan/bootstrap-datetimepicker/issues/1128", $DTE.vsNavigateOptions.vsNavigateOptionsNewWindow)
->>>>>>> parent of 9ba993f... Merge branch 'master' into Forman
